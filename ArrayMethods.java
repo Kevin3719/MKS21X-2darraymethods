c8e42a88c21e175312f26b2c0f800ff1050ec14c
Kevin Chen <kchen01@stuy.edu>
2018-11-06 18:11:33 -0500

    added part 2

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -33,7 +33,23 @@ public class ArrayMethods{
   }
  //returns an array of the row sums for each row of ary.
  //Index i of the return array contains the sum of elements in row i.
+public static int maxlength(int[][] ary) {
+  int output = 0;
+  for {int i = 0; i < ary.length; i += 1} {
+    if (output < ary[x].length) {
+      output = ary[x].length}
+    }
+    return output;
+  }
 
+ public static int[] allColSums(int[][] ary){
+   int[] output = new int[maxlength(ary)];
+   for (int i = 0; i < output.length; i += 1) {
+     output[i] = columnSum(ary, i);
+   }
+   return output;
+ }
+     //Returns an array with the column sum of each column of ary.
 
 
 
